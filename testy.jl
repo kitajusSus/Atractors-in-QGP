eb4ac9cb2b1fc83bf2e3e5dcd3fec6e2d639af69
kitajuSus <kitaj2458@gmail.com>
2025-06-27 19:45:24 +0200

    POprawione testy

--- testy.jl
+++ testy.jl
@@ -1,6 +1,10 @@
 
 using DifferentialEquations
 using Plots
+using LaTeXStrings
+using Plots
+plotlyjs()
+
 
 # --- Parametry fizyczne ---
 const Cη = 1 / (4π)             # eta/s
@@ -14,7 +18,7 @@ function attractor_eq!(dA, A, p, w)
 end
 
 # --- Zakres zmiennej w (tau * T) ---
-wspan = (0.2, 5)
+wspan = (1, 50)
 
 # --- Warunki początkowe (kilka trajektorii) ---
 A0_list = [-0.5, 0.0, 1.0, 3.0, 4.0, -50, 10]
@@ -28,17 +32,34 @@ end
 
 # --- Tworzenie wykresu ---
 plt = plot(size=(800,600))
-colors = [:blue, :blue, :blue, :blue, :blue, :blue, :orange]
+colors = [:blue, :green, :blue, :blue, :blue, :red, :blue]
 
 for (i, sol) in enumerate(sols)
     plot!(plt, sol.t, [u[1] for u in sol.u], label="A₀ = $(A0_list[i])", color=colors[i], lw=2)
 end
 
+a0_list = [-50 + 0.5*i for i in 1:500]
+
+sols2 = []
+for a0 in a0_list
+    prob2 = ODEProblem(attractor_eq!,[a0],wspan)
+    sol2 = solve(prob2,Tsit5(), abstol=1e-8, reltol = 1e-8)
+    push!(sols2,sol2)
+end
+
+
+kolory = [:violet]
+# inne warianty
+for (i,sol2) in enumerate(sols2)
+    plot!(plt, sol2.t, [u[1] for u in sol2.u], label ="a_0 = $(a0_list[i])", color =:violet, lw=2)
+    
+end
+
 xlabel!(plt, "w = τ·T")
 ylabel!(plt, "A(w)")
 title!(plt, "Ewolucja A(w) dla różnych warunków początkowych")
-
+display(plt)
 # --- Zapis do pliku PNG ---
-savefig(plt, "attractor_plot1.png")
-println("Wykres zapisany jako attractor_plot.png")
+# savefig(plt, "attractor_plot1.png")
+# println("Wykres zapisany jako attractor_plot.png")
 
