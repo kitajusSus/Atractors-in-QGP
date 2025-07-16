47930cbf141a1c1447fed623339679398628b4c2
kitajusSusWWA <kitaj2458@gmail.com>
2025-07-16 20:49:22 +0200

    popriawine ale dalej wymaga szliwoawania

--- atraktor/A_and_T_evolution.jl
+++ atraktor/A_and_T_evolution.jl
@@ -1,147 +1,119 @@
-# --- Załadowanie potrzebnych pakietów ---
+# 1. Importowanie potrzebnych pakietów
 using DifferentialEquations
 using Plots
 using Printf
 
-# Ustawienie domyślnego backendu na GR (szybki)
-gr()
+# 2. Definicja stałych fizycznych
+# Używamy wartości dla teorii N=4 SYM jako przykładu (z artykułu przeglądowego, równanie 33)
+# Zapewniają one realistyczne zachowanie atraktora.
+const C_η = 1 / (4π)
+const C_τπ = (2 - log(2)) / (2π)
+const C_λ1 = 1 / (2π)
+const PARAMS = (C_η, C_τπ, C_λ1)
 
-# --- Definicje fizyczne ---
-const Cη = 1 / (4π)
-const Cτ = (2 - log(2)) / (2π)
-const ħc = 197.3  # Stała konwersji MeV·fm
+# 3. Definicja układu równań różniczkowych (ODE)
+# u = [T, A] - wektor stanu (Temperatura, Anizotropia)
+# p - parametry fizyczne
+# τ - czas właściwy
+function hydro_evolution!(du, u, p, τ)
+    T, A = u
+    C_η, C_τπ, C_λ1 = p
 
-# --- Układ równań różniczkowych (ewolucja w czasie τ) ---
-# To jest serce naszej symulacji.
-# u[1] -> A (anizotropia), u[2] -> T (temperatura w MeV)
-function attractor_system!(du, u, p, τ)
-    A, T = u
+    # Równanie na d(T)/dτ (z τ d(logT)/dτ = -1/3 + A/18)
+    du[1] = (T / τ) * (-1/3 + A / 18)
 
-    # Zmienna pomocnicza w. Zabezpieczenie przed dzieleniem przez zero.
-    w = τ * T / ħc
-    if w < 1e-12
-        w = 1e-12
-    end
-
-    # Równanie na dT/dτ
-    # Zabezpieczenie przed A ≈ -12
-    denom_T = τ * (A + 12.0)
-    if abs(denom_T) < 1e-12
-        denom_T = sign(denom_T) * 1e-12
-    end
-    dT_dτ = T * (A - 6.0) / denom_T
-    du[2] = dT_dτ
-
-    # Równanie na dA/dτ (z reguły łańcuchowej)
-    # 1. Obliczamy dA/dw
-    denom_A_dw = Cτ * (1.0 + A / 12.0)
-    if abs(denom_A_dw) < 1e-12
-        denom_A_dw = sign(denom_A_dw) * 1e-12
-    end
-    num_A_dw = (3.0 / 2.0) * (8.0 * Cη / w - A) - (Cτ / (3.0 * w)) * A^2
-    dA_dw = num_A_dw / denom_A_dw
-
-    # 2. Obliczamy dw/dτ
-    dw_dτ = (T + τ * dT_dτ) / ħc
-    
-    # 3. Łączymy wszystko
-    du[1] = dA_dw * dw_dτ
+    # Równanie na d(A)/dτ (z teorii MIS)
+    # Zmieniona postać równania (34) z artykułu
+    term_T_dep = τ * T * (A + (C_λ1 / (12 * C_η)) * A^2)
+    term_A2_dep = (2/9) * C_τπ * A^2
+    du[2] = (1 / (C_τπ * τ)) * (8*C_η - term_T_dep - term_A2_dep)
 end
 
-# --- Parametry symulacji ---
-const N_POINTS = 10     # Liczba symulowanych trajektorii
-const τ_initial = 0.2      # Początkowy czas fizyczny [fm/c]
-const τ_final = 1.5        # Końcowy czas fizyczny [fm/c]
+# 4. Parametry symulacji
+const N_POINTS = 500         # Liczba generowanych warunków początkowych
+const T_RANGE = (200.0, 500.0) # Zakres początkowej temperatury T [MeV]
+const A_RANGE = (-1.0, 5.0)   # Zakres początkowej anizotropii A
 
-# Zakresy dla losowych warunków początkowych
-const T_range = (150.0, 400.0) # [MeV]
-const A_range = (-2.0, 5.0)
+const τ_start = 0.2           # Czas początkowy [fm/c]
+const τ_end = 1.2             # Czas końcowy [fm/c]
+const τ_span = (τ_start, τ_end)
 
-# --- Pętla rozwiązująca ---
-println("Generowanie i rozwiązywanie $N_POINTS trajektorii...")
-solutions = []
+# Momenty czasu, dla których będziemy tworzyć wykresy ("migawki")
+const τ_snapshots = [0.2, 0.4, 0.6, 0.8, 1.0, 1.2]
+
+# 5. Generowanie warunków początkowych i rozwiązywanie ODE
+println("Rozpoczynam symulację ewolucji dla $N_POINTS warunków początkowych...")
+
+# Słownik do przechowywania wyników dla każdej migawki czasu
+# Klucz: czas τ, Wartość: (wektor T, wektor A, wektor T_początkowych do kolorowania)
+results = Dict{Float64, Tuple{Vector{Float64}, Vector{Float64}, Vector{Float64}}}()
+for τ_val in τ_snapshots
+    results[τ_val] = ([], [], [])
+end
+
+# Generujemy losowe warunki początkowe
+initial_conditions_T = T_RANGE[1] .+ (T_RANGE[2] - T_RANGE[1]) .* rand(N_POINTS)
+initial_conditions_A = A_RANGE[1] .+ (A_RANGE[2] - A_RANGE[1]) .* rand(N_POINTS)
+
+# Pętla po wszystkich warunkach początkowych
 for i in 1:N_POINTS
-    print("Rozwiązywanie trajektorii $i/$N_POINTS\r")
+    T₀ = initial_conditions_T[i]
+    A₀ = initial_conditions_A[i]
+    u₀ = [T₀, A₀]
 
-    # Losujemy stan początkowy (T₀, A₀) w czasie τ_initial
-    T₀ = rand(T_range)
-    A₀ = rand(A_range)
-    u₀ = [A₀, T₀]
+    # Definiowanie i rozwiązywanie problemu ODE
+    prob = ODEProblem(hydro_evolution!, u₀, τ_span, PARAMS)
+    
+    # Rozwiązujemy równanie, mówiąc solverowi, żeby zapisał wyniki w punktach τ_snapshots
+    sol = solve(prob, Tsit5(), saveat=τ_snapshots)
 
-    # Definiujemy problem do rozwiązania
-    τ_span = (τ_initial, τ_final)
-    prob = ODEProblem(attractor_system!, u₀, τ_span)
-
-    # Rozwiązujemy, używając odpornego solvera
-    # `save_everystep=false` i `dense=true` są kluczowe dla wydajności
-    # i dokładnej interpolacji później.
-    sol = solve(prob, Rodas5(autodiff=false), save_everystep=false, dense=true, abstol=1e-6, reltol=1e-6, maxiters=1e7)
-
-    # Dodajemy tylko pomyślnie zakończone rozwiązania
-    if sol.retcode == :Success
-        push!(solutions, sol)
+    # Zapisujemy wyniki dla każdej migawki
+    for (j, τ_val) in enumerate(τ_snapshots)
+        T_at_τ, A_at_τ = sol.u[j]
+        push!(results[τ_val][1], T_at_τ)
+        push!(results[τ_val][2], A_at_τ)
+        push!(results[τ_val][3], T₀) # Zapisujemy T₀ do kolorowania
+    end
+    
+    # Wyświetlanie postępu
+    if i % 50 == 0
+        println("... Obliczono $i / $N_POINTS trajektorii.")
     end
 end
-println("\nZakończono. Pomyślnie rozwiązano $(length(solutions)) trajektorii.")
 
-# --- Tworzenie animacji ---
-if isempty(solutions)
-    error("Nie udało się rozwiązać żadnej trajektorii. Nie można stworzyć animacji.")
-end
+println("Symulacja zakończona. Generowanie animacji...")
 
-# Definiujemy klatki czasowe dla naszej animacji
-τ_frames = range(τ_initial, τ_final, length=150)
+# 6. Tworzenie animacji z serii wykresów
+anim = @animate for τ_val in τ_snapshots
+    
+    # Pobieranie danych dla danego czasu
+    T_vals, A_vals, T0_vals = results[τ_val]
 
-println("Tworzenie animacji z $(length(τ_frames)) klatek...")
-anim = @animate for (i, τ_target) in enumerate(τ_frames)
-    print("Generowanie klatki $i/$(length(τ_frames))\r")
-
-    # Zbieramy współrzędne wszystkich punktów w tej klatce
-    points_T = Float64[]
-    points_A = Float64[]
-
-    for sol in solutions
-        # Sprawdzamy, czy ten moment czasowy jest w zakresie danego rozwiązania
-        if τ_initial <= τ_target <= sol.t[end]
-            # Używamy interpolacji `sol(τ_target)`, aby uzyskać dokładne wartości
-            A_target, T_target = sol(τ_target)
-            
-            # Dodajemy tylko sensowne punkty do narysowania
-            if isfinite(A_target) && isfinite(T_target)
-                push!(points_A, A_target)
-                push!(points_T, T_target)
-            end
-        end
-    end
-
-    # Rysujemy klatkę (za każdym razem od nowa)
-    plot(
-        size=(800, 600),
-        title="Przestrzeń Stanów (T, A) w τ = $(@sprintf("%.2f", τ_target)) fm/c",
-        xlabel="Temperatura T [MeV]",
-        ylabel="Anizotropia A",
-        xlims=(0, 500),
-        ylims=(-2, 5),
-        legend=false,
-        grid=true
+    # Tworzenie pojedynczego wykresu (jednej klatki animacji)
+    scatter(T_vals, A_vals,
+        title = @sprintf("Przestrzeń Stanów (T, A) w τ = %.2f fm/c", τ_val),
+        xlabel = "Temperatura T [MeV]",
+        ylabel = "Anizotropia A",
+        label = "",
+        xlims = (0, T_RANGE[2]), # Stałe granice osi dla lepszego porównania
+        ylims = (A_RANGE[1], A_RANGE[2]),
+        markersize = 3,
+        markerstrokewidth = 0,
+        alpha = 0.7,
+        zcolor = T0_vals, # Kolorowanie punktów wg ich początkowej temperatury
+        colorbar_title = " T₀ [MeV]",
+        framestyle = :box
     )
-
-    # Rysujemy punkty, jeśli jakieś mamy
-    if !isempty(points_T)
-        scatter!(
-            points_T, 
-            points_A, 
-            marker_z=points_T, # Koloruj punkty wg temperatury
-            color=:viridis,
-            clims=T_range,
-            markersize=4,
-            markerstrokewidth=0
-        )
-    end
 end
 
-# Zapisanie gotowej animacji jako plik GIF
-gif_filename = "attractor_evolution_final_clean.gif"
-gif(anim, gif_filename, fps=20)
+# Zapisywanie animacji do pliku GIF
+gif(anim, "ewolucja_atraktora.gif", fps = 1)
 
-println("\nAnimacja została pomyślnie zapisana jako: $gif_filename")
+println("\nAnimacja została zapisana do pliku 'ewolucja_atraktora.gif'")
+
+# Opcjonalnie: można też wygenerować osobne pliki dla każdego wykresu
+# for τ_val in τ_snapshots
+#     T_vals, A_vals, T0_vals = results[τ_val]
+#     p = scatter(T_vals, A_vals, ..., ) # jak wyżej
+#     savefig(p, "stan_w_tau_$(replace(string(τ_val), "." => "_")).png")
+# end
