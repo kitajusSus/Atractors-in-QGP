03934ba574f0e91851869a219df26ffd29de3fee
kitajusSus <116976902+kitajusSus@users.noreply.github.com>
2026-04-21 19:35:11 +0200

    testing

--- examples/lle_examples/eigen_value.jl
+++ examples/lle_examples/eigen_value.jl
@@ -1,9 +1,9 @@
 using LinearAlgebra
 include("../ncbj_lle.jl")
 using GLMakie
+include("swissroll.jl")
 
-
-function ex_eigen_value(; N=2000, K=40, indeksy = 1:10)
+function ex_eigen_value(; N=2000, K=40, indeksy = 2:10)
     X, labels = swissroll_dane(N)
     
     W = ncbj4_lle_basic(X, K)
@@ -12,12 +12,12 @@ function ex_eigen_value(; N=2000, K=40, indeksy = 1:10)
     F = eigen(Symmetric(M))
     wartosci_wlasne = F.values
     wartosci_do_wykresu = wartosci_wlasne[indeksy]
-
+    return wartosci_do_wykresu ./ wartosci_do_wykresu[1]
     # fig = Figure()
     # ax = Axis(fig[1, 1], title = "Wartości własne", xlabel = "Indeks", ylabel = "Wartosc")
     # scatterlines!(ax, indeksy, wartosci_do_wykresu, markersize=10)
     # display(fig)
-    return W, F.values
+    # return W, F.values, wartosci_do_wykresu, wartosci_wlasne
 
 end
 
--- examples/lle_examples/swissroll.jl
+++ examples/lle_examples/swissroll.jl
@@ -4,27 +4,32 @@ const dx = 1e-3
 include("../ncbj_lle.jl")
 # skopiowane z examples/ncbj_lle.jl
 include("plots_lle.jl")
+using Random 
+
 
 function swissroll_dane(N)
-    τ = 1.5π .+ 3π .* rand(Float32, N)
-    h = 21f0 .* rand(Float32, N)
-    r = τ .+ (0.2f0 .* randn(Float32, N))
+
+    rnd = Xoshiro(5)
+    τ = 1.5π .+ 3π .* rand(Xoshiro(5), Float32, N)
+    h = 21f0 .* rand(Xoshiro(5), Float32, N)
+    r = τ .+ (0.2f0 .* randn(Xoshiro(5), Float32, N))
     X = [r .* cos.(τ) h r .* sin.(τ)]'
     return X, τ
 end
 
 function ex_swissroll(; N=2000, K=12)
+    
     X, labels = swissroll_dane(N)
     # println("Wygenerowane dane Swiss Roll (X) \n  ", X )
     wagi = ncbj4_lle_basic(X, K)
     # println("Macierz wag W:\n", wagi)
     Y = ncbj5_nowy_manifold(wagi)
     # println("Nowe Wektory Y (2D):\n", Y)
-    fig = plot_examples_lle(X, Y, labels)
-    display(fig)
-    print("Press Enter to continue...")
-    readline()
-    return fig
+    # fig = plot_examples_lle(X, Y, labels)
+    # display(fig)
+    # print("Press Enter to continue...")
+    # readline()
+    return Y
 end
 
 
