957c725663091f150006d2b4f750752e6c6e8a3f
kitajusSus <kitaj2458@gmail.com>
2026-04-23 08:22:00 +0200

    Funkcja Do generowania tabeli takie jak robilismy w repl examples/lle_example/generowanie_tabeli.jl

--- matlab/lle/lle.m
+++ matlab/lle/lle.m
@@ -8,15 +8,10 @@
 % Y = embedding as dmax x N matrix
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-function [Y,W] = lle(X,K,d)
+function [Y,W,E] = lle(X,K,d)
 
 [D,N] = size(X);
 fprintf(1,'LLE running on %d points in %d dimensions\n',N,D);
-
-
-% STEP1: COMPUTE PAIRWISE DISTANCES & FIND NEIGHBORS 
-fprintf(1,'-->Finding %d nearest neighbours.\n',K);
 
 X2 = sum(X.^2,1);
 distance = repmat(X2,N,1)+repmat(X2',1,N)-2*X'*X;
@@ -24,32 +19,21 @@ distance = repmat(X2,N,1)+repmat(X2',1,N)-2*X'*X;
 [sorted,index] = sort(distance);
 neighborhood = index(2:(1+K),:);
 
-
-
-% STEP2: SOLVE FOR RECONSTRUCTION WEIGHTS
-fprintf(1,'-->Solving for reconstruction weights.\n');
-
 if(K>D) 
-  fprintf(1,'   [note: K>D; regularization will be used]\n'); 
-  tol=1e-3; % regularlizer in case constrained fits are ill conditioned
+  tol=1e-3;
 else
   tol=0;
 end
 
 W = zeros(K,N);
 for ii=1:N
-   z = X(:,neighborhood(:,ii))-repmat(X(:,ii),1,K); % shift ith pt to origin
-   C = z'*z;                                        % local covariance
-   C = C + eye(K,K)*tol*trace(C);                   % regularlization (K>D)
-   W(:,ii) = C\ones(K,1);                           % solve Cw=1
-   W(:,ii) = W(:,ii)/sum(W(:,ii));                  % enforce sum(w)=1
+   z = X(:,neighborhood(:,ii))-repmat(X(:,ii),1,K);
+   C = z'*z;                                        
+   C = C + eye(K,K)*tol*trace(C);                   
+   W(:,ii) = C\ones(K,1);                           
+   W(:,ii) = W(:,ii)/sum(W(:,ii));                  
 end;
 
-
-% STEP 3: COMPUTE EMBEDDING FROM EIGENVECTS OF COST MATRIX M=(I-W)'(I-W)
-fprintf(1,'-->Computing embedding.\n');
-
-% M=eye(N,N); % use a sparse matrix with storage for 4KN nonzero elements
 M = sparse(1:N,1:N,ones(1,N),N,N,4*K*N); 
 for ii=1:N
    w = W(:,ii);
@@ -59,14 +43,12 @@ for ii=1:N
    M(jj,jj) = M(jj,jj) + w*w';
 end;
 
-% CALCULATION OF EMBEDDING
 options.disp = 0; options.isreal = 1; options.issym = 1; 
 [Y,eigenvals] = eigs(M,d+1,"sm");
-Y = Y(:,1:2)'*sqrt(N); % bottom evect is [1,1,1,1...] with eval 0
-diag(eigenvals)
+Y = Y(:,1:2)'*sqrt(N);
+E = diag(eigenvals);
 
 fprintf(1,'Done.\n');
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
