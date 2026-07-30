ca675c7e85da6ae5431d539bfcd6dfe36965f19f
kitajusSus <116976902+kitajusSus@users.noreply.github.com>
2026-07-30 19:23:39 +0200

    jazdaaaaa usuwanie całego bagna

--- docs/build/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"index.html","page":"Home","title":"Home","text":"Poniżej znajduje się automatycznie wygenerowana lista wszystkich funkcji w pakiecie:\n\n```@autodocs Modules = [AtractorsQGP]","category":"section"}]
-}
